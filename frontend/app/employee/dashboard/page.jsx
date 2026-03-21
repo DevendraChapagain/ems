bd882ac9be4e9edcd584d8401187235299227e3c
Devendra <devendrachapagain100@gmail.com>
2026-03-21 23:08:13 +0545

    Employee page

--- frontend/app/employee/dashboard/page.jsx
+++ frontend/app/employee/dashboard/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
      <div className="flex items-center justify-center min-h-screen bg-[#111522]">
-      <div className='text-5xl text-white'> Welcome </div>
+      <div className='text-5xl text-white'> Welcome to Kinetic EMS </div>
       
      </div>
   )
