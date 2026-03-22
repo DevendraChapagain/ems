2295b9ee95ebdeeb8dcf6b16d76effb70175cea5
Devendra <devendrachapagain100@gmail.com>
2026-03-22 18:54:15 +0545

    feat(db): added the task model

---
+++ Backend/models/task.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+
+    descriptiom: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+
+    assignedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Task must by admin"],
+    },
+
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Task must be assigned to a user"],
+    },
+
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "low",
+    },
+
+    taskStatus: {
+      type: String,
+      enum: ["pending", "onprogress", "completed"],
+      default: "pending",
+    },
+
+    dueDate: {
+      type: Date,
+      required: [true, "Due Date is required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Task = mongoose.model("Task",taskSchema);
+export default Task;
